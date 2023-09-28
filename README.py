1e739a8685d06bfdef2b156b780813e61b1e41d0
Ahmed Cheema <51097919+ahmed-cheema@users.noreply.github.com>
2023-09-28 17:12:05 -0500

    Delete README.py

--- README.py
+++
@@ -1,6 +0,0 @@
-# Once the ".m" files are used to calculate texton histograms, line features, and gist descriptors, the following Python files should be run (in order):
-
-# preprocessing.ipynb - contains code for splitting data based on source
-# features.ipynb - calculate remaining features, run 1-NN feature predictions
-# preparation.ipynb - splits into validation set, saves arrays used for final modeling
-# modeling.py - contains convolutional neural network code
